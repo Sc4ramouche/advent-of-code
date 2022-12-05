3bba7085033a77860981d622d5ca5117c1891ad4
sc4ramouche <vladislav.kovechenkov@gmail.com>
2022-12-05 23:19:22 +0100

    feat: day_5 solution

---
+++ day_5/day_5.js
@@ -0,0 +1,36 @@
+const getInput = require('../utils/getInput').getInput;
+
+const input = getInput('day_5/input.txt');
+
+const crates = [
+  ['D', 'H', 'N', 'Q', 'T', 'W', 'V', 'B'],
+  ['D', 'W', 'B'],
+  ['T', 'S', 'Q', 'W', 'J', 'C'],
+  ['F', 'J', 'R', 'N', 'Z', 'T', 'P'],
+  ['G', 'P', 'V', 'J', 'M', 'S', 'T'],
+  ['B', 'W', 'F', 'T', 'N'],
+  ['B', 'L', 'D', 'Q', 'F', 'H', 'V', 'N'],
+  ['H', 'P', 'F', 'R'],
+  ['Z', 'S', 'M', 'B', 'L', 'N', 'P', 'H'],
+];
+
+const first = (arr) => arr[0];
+const last = (arr) => arr[arr.length - 1];
+
+const regexpDigits = /(\d+)/gm;
+function readInput(line) {
+  const [stack, previous, next] = line.matchAll(regexpDigits);
+
+  return [stack, previous, next].map(first).map(Number);
+}
+
+for (const line of input.split('\n').map(readInput)) {
+  const [stack, previous, next] = line;
+
+  for (let i = 0; i < stack; i++) {
+    const crate = crates[previous - 1].pop();
+    crates[next - 1] = crates[next - 1].concat(crate);
+  }
+}
+
+console.log(crates.reduce((acc, line) => acc + last(line), ''));
